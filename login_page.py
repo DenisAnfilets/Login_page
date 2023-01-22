277f6cfd5fa1709c4629d0a57737d14273bd05fd
DenisAnfilets <112340250+DenisAnfilets@users.noreply.github.com>
2023-01-22 12:46:33 +0300

    Add files via upload

--- login_page.py
+++ login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+import time
 
 
 class LoginPage(BasePage):
@@ -14,3 +15,4 @@ class LoginPage(BasePage):
     def go_to_button(self):
         login_button = self.browser.find_element(*LoginPageLocators.LOGIN_BUTTON)
         login_button.submit()
+        time.sleep(3)
